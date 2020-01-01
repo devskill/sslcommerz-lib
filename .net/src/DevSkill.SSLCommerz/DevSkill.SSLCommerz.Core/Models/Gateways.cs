b34e129cff6336507e33442884d049027ab3ef42
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-01-01 14:06:06 +0600

    Created required Models for SSLCommerz DTO

---
+++ .net/src/DevSkill.SSLCommerz/DevSkill.SSLCommerz.Core/Models/Gateways.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace SSLCommerzSample.Models
+{
+	public class Gateways
+	{
+		[JsonProperty("visa")]
+		public string Visa { get; set; }
+		[JsonProperty("master")]
+		public string MasterCard { get; set; }
+		[JsonProperty("Amex")]
+		public string AmericanExpress { get; set; }
+		[JsonProperty("othercards")]
+		public string OtherCards { get; set; }
+		[JsonProperty("internetbanking")]
+		public string InternetBanking { get; set; }
+		[JsonProperty("mobilebanking")]
+		public string MobileBanking { get; set; }
+	}
+}
