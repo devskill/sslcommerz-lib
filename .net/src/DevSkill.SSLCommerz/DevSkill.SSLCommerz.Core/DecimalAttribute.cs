bc70f9a6a0be5f6f2d1a78c4799656da63208859
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-01-02 01:28:17 +0600

    Changed namespace

--- .net/src/DevSkill.SSLCommerz/DevSkill.SSLCommerz.Core/DecimalAttribute.cs
+++ .net/src/DevSkill.SSLCommerz/DevSkill.SSLCommerz.Core/DecimalAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace SSLCommerzSample.Core
+namespace DevSkill.SSLCommerz.Core
 {
 	public class DecimalAttribute 
 		: RegularExpressionAttribute
