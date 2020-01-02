f83ccd827794bed1645d7fcdb5082f8db618ea3c
rafsanulhasan <rafsanulhasan@outlook.com>
2020-01-02 16:42:51 +0600

    Buillt Module for Autofac and Written extension for registration

--- .net/src/DevSkill.SSLCommerz/DevSkill.Extensions.SSLCommerz.DependencyInjection/AutofacExtensions.netcore31.cs
+++ .net/src/DevSkill.SSLCommerz/DevSkill.Extensions.SSLCommerz.DependencyInjection/AutofacExtensions.netcore31.cs
@@ -1,9 +1,15 @@
 ﻿using Autofac;
+using Autofac.Core.Registration;
+using DevSkill.SSLCommerz.Core;
 
 namespace DevSkill.Extensions.SSLCommerz.DependencyInjection
 {
 	public static class AutofacExtensions
 	{
-		public static void a(this string k) { }
+		public static IModuleRegistrar RegisterSSLCommerzPaymentModule(
+			this ContainerBuilder cb,
+			SSLCommerzSettings settings
+			)
+			=> cb.RegisterModule(new SSLCommerzModule(settings));
 	}
 }
