67fc285a1e63a6bcae57e8a909f97bb57463de4f
rafsanulhasan <rafsanulhasan@outlook.com>
2020-01-03 17:26:55 +0600

    Merged 5 files into one

---
+++ .net/src/DevSkill.SSLCommerz/DevSkill.Extensions.SSLCommerz.DependencyInjection/AutofacExtensions.common.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Core.Registration;
+
+using DevSkill.SSLCommerz.Core;
+
+using System;
+
+namespace DevSkill.Extensions.SSLCommerz.DependencyInjection
+{
+	public static class AutofacExtensions
+	{
+		public static IModuleRegistrar RegisterSSLCommerzPaymentModule(
+			this ContainerBuilder cb,
+			SSLCommerzApiSettings apiSettings,
+			SSLCommerzUrlSettings urlSettings,
+			SSLCommerzSettings settings
+			)
+			=> cb.RegisterModule(new SSLCommerzModule(settings, apiSettings, urlSettings, 4));
+
+		public static IModuleRegistrar RegisterSSLCommerzPaymentModule(
+			this ContainerBuilder cb,
+			Action<SSLCommerzApiSettings> apiSettingsDelegate,
+			Action<SSLCommerzUrlSettings> urlSettingsDelegate,
+			SSLCommerzSettings settings
+			)
+			=> cb.RegisterModule(new SSLCommerzModule(settings, apiSettingsDelegate, urlSettingsDelegate, 4));
+	}
+}
