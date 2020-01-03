b6f7a7c81e76f63f7ddf177fc76d1a1daf0ccb75
rafsanulhasan <rafsanulhasan@outlook.com>
2020-01-03 17:28:11 +0600

    Created/Updated Attribute

--- .net/src/DevSkill.SSLCommerz/DevSkill.SSLCommerz.Core/DecimalAttribute.cs
+++ .net/src/DevSkill.SSLCommerz/DevSkill.SSLCommerz.Core/DecimalAttribute.cs
@@ -2,9 +2,10 @@
 
 namespace DevSkill.SSLCommerz.Core
 {
-	public class DecimalAttribute 
+	public class DecimalAttribute
 		: RegularExpressionAttribute
 	{
+		public int NumberOfDigits { get; set; }
 		public int DecimalPlaces { get; set; }
 
 		public DecimalAttribute(int decimalPlaces)
@@ -13,13 +14,25 @@ namespace DevSkill.SSLCommerz.Core
 				 "This number can have maximum {0} decimal places"
 			)
 		{
+			NumberOfDigits = 0;
+			DecimalPlaces = decimalPlaces;
+		}
+
+		public DecimalAttribute(int numberOfDigits, int decimalPlaces)
+		    : this(
+				 numberOfDigits,
+				 decimalPlaces,
+				 "This number can have maximum {0} decimal places with maximum {1} digit(s)"
+			)
+		{
+			NumberOfDigits = numberOfDigits;
 			DecimalPlaces = decimalPlaces;
 		}
 
 		public DecimalAttribute(
 			int decimalPlaces,
 			string errorMessage)
-		    : base($@"^\d*\.?\d{{0,{decimalPlaces}}}$")
+		    : base($@"^[1-9]\d+(\.\d{{{2}}})?$")
 		{
 			DecimalPlaces = decimalPlaces;
 			if (string.IsNullOrEmpty(errorMessage))
@@ -27,10 +40,31 @@ namespace DevSkill.SSLCommerz.Core
 			ErrorMessage = errorMessage;
 		}
 
-		public override string FormatErrorMessage(string name) 
-			=> string.Format(
+		public DecimalAttribute(
+			int numberOfDigits,
+			int decimalPlaces,
+			string errorMessage)
+		    : base($@"^[1-9]\d{{{numberOfDigits}}}(\.\d{{{decimalPlaces}}})?$")
+		{
+			NumberOfDigits = numberOfDigits;
+			DecimalPlaces = decimalPlaces;
+			if (string.IsNullOrEmpty(errorMessage))
+				errorMessage = "This number can have maximum {0} decimal places with maximum {1} digits(s)";
+			ErrorMessage = errorMessage;
+		}
+
+		public override string FormatErrorMessage(string name)
+		{
+			if (NumberOfDigits > 0)
+				return string.Format(
+				   ErrorMessage,
+				   DecimalPlaces,
+				   NumberOfDigits
+				);
+			return string.Format(
 				ErrorMessage,
 				DecimalPlaces
 			);
+		}
 	}
 }
---
+++ .net/src/DevSkill.SSLCommerz/DevSkill.SSLCommerz.Core/NumericAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSkill.SSLCommerz.Core
+{
+	public class NumericAttribute 
+		: RegularExpressionAttribute
+	{
+		public int DecimalPlaces { get; set; }
+
+		public NumericAttribute()
+		    : this(
+				 "This string must contain only numerical characters"
+			)
+		{
+		}
+
+		public NumericAttribute(
+			string errorMessage
+		)
+		    : base($@"^[1-9]\d*$")
+		{
+			if (string.IsNullOrEmpty(errorMessage))
+				errorMessage = "This string must contain only numerical characters";
+			ErrorMessage = errorMessage;
+		}
+	}
+}
