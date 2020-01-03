e29de05bf49ac969d634e825de0b4e4e96d9f172
rafsanulhasan <rafsanulhasan@outlook.com>
2020-01-03 17:27:16 +0600

    Added model for cart

---
+++ .net/src/DevSkill.SSLCommerz/DevSkill.SSLCommerz.Core/Models/Cart.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DevSkill.SSLCommerz.Core.Models
+{
+	[JsonObject]
+	public class Cart
+	{
+		[Required]
+		[JsonProperty(
+			PropertyName = "product",
+			Required = Required.Always
+		)]
+		public string Name { get; set; }
+
+		[Required]
+		[Numeric]
+		[JsonProperty(
+			PropertyName = "quantity",
+			Required = Required.Always
+		)]
+		public string Quantity { get; set; }
+
+		[Required]
+		[Decimal(12, 2)]
+		[JsonProperty(
+			PropertyName = "amount",
+			Required = Required.Always
+		)]
+		public decimal Amount { get; set; }
+	}
+}
